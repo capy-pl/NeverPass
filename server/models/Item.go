c213e651a854237658be54bbe99ccbf5a0af6fd2
PhilipL <honesty1997@gmail.com>
2020-12-17 00:08:45 +0800

    specify json field name for model attributes

--- server/models/Item.go
+++ server/models/Item.go
@@ -6,7 +6,7 @@ import "gorm.io/gorm"
 type Item struct {
 	Type      Type        `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"type"`
 	TypeID    int         `json:"typeId"`
-	Creator   User        `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
+	Creator   User        `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"creator"`
 	Status    int         `gorm:"default:1;NOT NULL" json:"status"` // 1 stands for available. 0 stands for deleted.
 	Values    []ItemField `gorm:"many2many:item_item_fields;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"values"`
 	CreatorID int         `gorm:"index" json:"creatorId"`
--- server/models/ItemField.go
+++ server/models/ItemField.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 // ItemField represents the field value of a single item in the vault.
 type ItemField struct {
 	Value             string          `gorm:"not null" json:"value"`
-	FieldDefinition   FieldDefinition `gorm:"constraints:OnUpdate:CASCADE,OnDelete:SET NULL"`
+	FieldDefinition   FieldDefinition `gorm:"constraints:OnUpdate:CASCADE,OnDelete:SET NULL" json:"fieldDefinition"`
 	FieldDefinitionID int             `json:"fieldDefinitionId"`
 	gorm.Model
 }
