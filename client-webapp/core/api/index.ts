63aee9bdae4ade1d98dfd8d87d676958d1a8a593
PhilipL <honesty1997@gmail.com>
2020-12-17 00:12:29 +0800

    add universal request and response interface

---
+++ client-webapp/core/api/index.ts
@@ -0,0 +1,37 @@
+import { Type, Item } from '../../model';
+
+export interface UserCreateRequest {
+  account: string;
+  password: string;
+  pk: string;
+}
+
+export interface UserCreateResponse {
+  id: number;
+}
+
+export interface UserSignInRequest {
+  account: string;
+  password: string;
+}
+
+export interface UserSignInResponse {
+  token: string;
+}
+
+export interface UserInfoResponse {
+  id: string;
+  pk: string;
+  account: string;
+}
+
+export type GetTypesResponse = Type[];
+export type GetTypeResponse = Type;
+
+export type GetItemsResponse = Item[];
+export type GetItemResponse = Item;
+
+export type AddItemRequest = {
+  typeid: number;
+  fields: { [key: string]: string };
+};
