364065f95e29aa564233a829dbfc1d4217de52ed
PhilipL <honesty1997@gmail.com>
2020-12-17 00:11:58 +0800

    add util functions

---
+++ client-webapp/core/util.ts
@@ -0,0 +1,8 @@
+export function validateEmail(email: string): Boolean {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+export function capitalize(message: string): string {
+  return `${message.charAt(0).toUpperCase()}${message.slice(1)}`;
+}
